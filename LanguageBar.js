cb9041dc8247b1295ca09a0688174397a07cee2b
michaelx-corner <mih344372252@126.com>
2018-04-16 17:07:38 +0800

    Create LanguageBar.js

---
+++ LanguageBar.js
@@ -0,0 +1,14 @@
+function GetUrlRelativePath()
+{
+  var url = document.location.toString();
+  var arrUrl = url.split("//");
+
+  var start = arrUrl[1].indexOf("/");
+  var relUrl = arrUrl[1].substring(start);
+
+  //if(relUrl.indexOf("?") != -1)
+  //{
+  //  relUrl = relUrl.split("?")[0];
+  //}
+  return relUrl;
+}
