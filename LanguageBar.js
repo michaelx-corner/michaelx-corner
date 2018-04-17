782d89f960fca3f46084a0ff6b6fbd8d0259b8c1
michaelx-corner <mih344372252@126.com>
2018-04-17 09:19:39 +0800

    Update LanguageBar.js

--- LanguageBar.js
+++ LanguageBar.js
@@ -2,11 +2,11 @@ var loc = GetUrlRelativePath();
 alert(loc);
 var loc_en = "./en" + loc;
 var loc_cn = "./zh-CN" + loc;
-document.write("<div style="text-align:right; font-size:0.75em"><span><a href=");
+document.write("<div style=\"text-align:right; font-size:0.75em\"><span><a href=\"");
 document.write(loc_cn);
-document.write(">CN（中文）</a></span> | <span><a href=");
+document.write("\">CN（中文）</a></span> | <span><a href=\"");
 document.write(loc_en);
-document.write(">EN(English)</a></span></div>");
+document.write("\">EN(English)</a></span></div>");
 
 function GetUrlRelativePath()
 {
